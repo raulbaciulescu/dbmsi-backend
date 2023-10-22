23f5ed4f842270b8de6a3ac53ae316964d121c2a
Raul Baciulescu <raulbaciulescu@gmail.com>
2023-10-22 19:29:37 +0300

    first commit

---
+++ src/main/java/com/university/dbmsibackend/DbmsiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.university.dbmsibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmsiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmsiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/university/dbmsibackend/DbmsiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.dbmsibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbmsiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
