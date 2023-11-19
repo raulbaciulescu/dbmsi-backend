a6c7f32bdc7545e7e4d204ee0179a14d42f53463
raulbaciulescu <raulbaciulescu@gmail.com>
2023-11-19 16:36:22 +0200

    validator changes

--- src/main/java/com/university/dbmsibackend/validator/DbsmiValidator.java
+++ src/main/java/com/university/dbmsibackend/validator/DbsmiValidator.java
@@ -3,10 +3,8 @@ package com.university.dbmsibackend.validator;
 import com.mongodb.client.MongoCollection;
 import com.university.dbmsibackend.domain.Database;
 import com.university.dbmsibackend.dto.InsertRequest;
-import com.university.dbmsibackend.dto.SelectAllResponse;
 import org.bson.Document;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -23,9 +21,7 @@ public class DbsmiValidator {
 
     public static boolean isValidRow(MongoCollection<Document> collection, InsertRequest request) {
         String primaryKey = request.key();
-        System.out.println("searched primary key: " + request.key());
         for (Document document : collection.find()) {
-            System.out.println(document.get("key").toString() + " " + document.get("value").toString());
             if (Objects.equals(primaryKey, document.get("key").toString()))
                 return false;
         }
