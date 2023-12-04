985ca7e0f6bac6b6e49ce1be075a1704bc754aa4
raulbaciulescu <raulbaciulescu@gmail.com>
2023-12-04 22:35:29 +0200

    simple select

--- src/main/java/com/university/dbmsibackend/service/QueryService.java
+++ src/main/java/com/university/dbmsibackend/service/QueryService.java
@@ -1,14 +1,115 @@
 package com.university.dbmsibackend.service;
 
+import com.mongodb.client.MongoClient;
+import com.university.dbmsibackend.domain.Table;
 import com.university.dbmsibackend.dto.QueryRequest;
+import com.university.dbmsibackend.dto.SelectAllResponse;
+import com.university.dbmsibackend.util.JsonUtil;
+import com.university.dbmsibackend.util.TableMapper;
 import lombok.AllArgsConstructor;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.select.*;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 @AllArgsConstructor
 public class QueryService {
+    private MongoClient mongoClient;
+    private MongoService mongoService;
+    private JsonUtil jsonUtil;
 
     public void executeQuery(QueryRequest request) {
         System.out.println(request.query());
+        String query = request.query();
+        try {
+            // Parse the SQL query
+            Statement statement = CCJSqlParserUtil.parse(query);
+
+            // Print the traversable tree
+            printSqlTree(statement);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void printSqlTree(Statement statement) throws Exception {
+        if (statement instanceof Select) {
+            Select select = (Select) statement;
+            Select selectBody = select.getSelectBody();
+            System.out.println(printSelectBodyTree(selectBody));
+        } else {
+            System.out.println("Not a SELECT statement.");
+        }
+    }
+
+    private List<Map<String, String>> printSelectBodyTree(Select selectBody) throws Exception {
+        String fromTableName;
+
+        if (selectBody instanceof PlainSelect) {
+            PlainSelect plainSelect = (PlainSelect) selectBody;
+
+            fromTableName = plainSelect.getFromItem().toString();
+            List<SelectItem<?>> selectItem = plainSelect.getSelectItems();
+            List<Map<String, String>> result = selectSimpleQuery(fromTableName, selectItem);
+
+//            // Print JOINs
+//            if (plainSelect.getJoins() != null) {
+//                for (Join join : plainSelect.getJoins()) {
+//                    printNode("Join", indentLevel);
+////                    printNode("JoinType: " + join.(), indentLevel + 1);
+//                    printNode("RightItem", indentLevel + 1);
+//                    printNode("Table: " + join.getRightItem(), indentLevel + 2);
+//                    printNode("OnExpression", indentLevel + 1);
+//                    printExpressionTree(join.getOnExpression(), indentLevel + 2);
+//                }
+//            }
+
+            // Print WHERE
+//            if (plainSelect.getWhere() != null) {
+//                printExpressionTree(plainSelect.getWhere(),);
+//            }
+
+            return result;
+        } else throw new Exception("DA");
+    }
+
+    /**
+     * {"id": 1, "name": "raul", "age": 21, "dadada": "DADADA"}
+     * @param fromTableName
+     * @param selectItems [name, age]
+     * @return
+     */
+    private List<Map<String, String>> selectSimpleQuery(String fromTableName, List<SelectItem<?>> selectItems) {
+        List<SelectAllResponse> rows = mongoService.selectAll("university", fromTableName);
+        Table table = jsonUtil.getTable(fromTableName, "university");
+        List<Map<String, String>> result = new ArrayList<>();
+        for (SelectAllResponse row: rows) {
+            Map<String, String> jsonRow = dictionaryToMap(TableMapper.mapKeyValueToTableRow(row.key(), row.value(), table));
+            Map<String, String> resultJson = new HashMap<>();
+            for (String key : jsonRow.keySet()) {
+                if (selectItems.contains(key))
+                    resultJson.put(key, jsonRow.get(key)); //{"name": "raul", "age": 21}
+            }
+            result.add(resultJson);
+        }
+
+        return result;
+    }
+
+    private static <K, V> Map<K, V> dictionaryToMap(Dictionary<K, V> dictionary) {
+        Map<K, V> map = new HashMap<>();
+
+        // Iterate over the keys in the Dictionary and add them to the Map
+        Enumeration<K> keys = dictionary.keys();
+        while (keys.hasMoreElements()) {
+            K key = keys.nextElement();
+            V value = dictionary.get(key);
+            map.put(key, value);
+        }
+
+        return map;
     }
 }
--- src/main/java/com/university/dbmsibackend/util/JsonUtil.java
+++ src/main/java/com/university/dbmsibackend/util/JsonUtil.java
@@ -3,11 +3,16 @@ package com.university.dbmsibackend.util;
 import com.google.gson.Gson;
 import com.google.gson.stream.JsonReader;
 import com.university.dbmsibackend.domain.Catalog;
+import com.university.dbmsibackend.domain.Database;
+import com.university.dbmsibackend.domain.Table;
+import com.university.dbmsibackend.exception.EntityNotFoundException;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Objects;
+import java.util.Optional;
 
 
 public class JsonUtil {
@@ -34,5 +39,21 @@ public class JsonUtil {
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public Table getTable(String tableName, String databaseName) {
+        Catalog catalog = getCatalog();
+        Optional<Database> optionalDatabase = catalog.getDatabases()
+                .stream()
+                .filter(db -> Objects.equals(db.getName(), databaseName))
+                .findFirst();
+        if (optionalDatabase.isPresent()) {
+            Optional<Table> optionalTable = optionalDatabase.get().
+                    getTables().stream()
+                    .filter(t -> Objects.equals(t.getName(), tableName)).findFirst();
+            if (optionalTable.isPresent())
+                return optionalTable.get();
+            else throw new EntityNotFoundException("Table doesn't exist!");
+        } else throw new EntityNotFoundException("Database doesn't exist!");
     }
 }
